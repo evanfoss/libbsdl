feec4f7660c689c91b8e6d45c554316535e1785b
evanfoss <evanfoss@gmail.com>
2015-02-02 00:55:17 -0500

    starting to layout the shape of the real preprocessor in comments

--- libbsdl.c
+++ libbsdl.c
@@ -12,12 +12,59 @@
 #include <stdlib.h>
 #include "libbsdl.h"
 
-/*
-extern void libbsdl_preprocessor(void)
+/* 
+ *
+ */
+extern void libbsdl_preprocessor(FILE *file)
 {
+
+	unsigned int location = 0;
+	size_t len = 0;
+
+	// create head node
+	
+	// call recursive function
+	
+	// is nead node address still a null pointer
+	// if yes then the file is empty it does not
+	// matter yes or know because we still return
+	// the same way.
+	free(line);
+	
 	return;
 }
-*/
+
+/*
+ * 
+ */
+void libbsdl_preprocessor_populate(FILE *file, size_t len)
+{
+	char *line = NULL;
+	ssize_t read;
+	int location;
+	read = getline(&line, &len, file);
+	if (read == -1)
+	{
+		free(line);
+		return;
+	}
+	// loop from 0 to the end of line minus the length of the string.
+	for (location = 0; location < read - 1; location++)
+	{
+		// if true then we found the comment
+		if (line[location] == '-' && line[location + 1] == '-')
+		{
+			printf("%s", line);
+		}
+		else if (line[location] =! ' ')
+		{
+			location == read;
+		}
+	}
+	free(line);
+	return;
+}
+
 
 /*
 extern void libbsdl_print_comment_block()
